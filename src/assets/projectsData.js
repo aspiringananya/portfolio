9d655280e185d96b02e7c24287e7ca4ab16acfdc
Ananya Banerjee <aspiringananya@gmail.com>
2021-07-20 13:10:52 +0530

    Updating the Project Contents

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -1,9 +1,13 @@
+import nameIt  from "./proj1.PNG";
+import flashType from "./proj2.PNG";
+import portfolio from "./proj3.PNG";
+
 export const projectsData = [
     {
         projectName: 'Name It!',
         projectDescription:
             'Do you find it difficult to name your projects or ventures? Fear not! Check out this cool name suggestion application which will help you find the best name for your next project, venture, or startup. Keep building!',
-        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj1.PNG',
+        imageUrl: nameIt,
         videoUrl:
             'https://github.com/aspiringananya/nameit/',
         projectUrl: 'https://aspiringananya.github.io/nameit/',
@@ -12,17 +16,17 @@ export const projectsData = [
         projectName: 'FlashType',
         projectDescription:
             'Having a good typing speed will always help you as a developer! Here, I present you the typing speed test application, where you can check your typing speed (in wpm) and share the results with your friends too.',
-        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj2.PNG',
+        imageUrl: flashType,
         videoUrl:
             'https://github.com/aspiringananya/flashtype/',
         projectUrl: 'https://aspiringananya.github.io/flashtype/',
     },
-    // {
-    //     projectName: 'Portfolio Website',
-    //     projectDescription:
-    //         'Showcase your skills/hobbies using your own Portfolio!!',
-    //     imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj5.PNG',
-    //     videoUrl: 'https://github.com/aspiringananya/portfolio/',
-    //     projectUrl: 'https://aspiringananya.github.io/portfolio/',
-    // },
+     {
+         projectName: 'Portfolio Website',
+         projectDescription:
+             'Showcase your skills/hobbies using your own Portfolio!!',
+        imageUrl: portfolio,
+         videoUrl: 'https://github.com/aspiringananya/portfolio/',
+        projectUrl: 'https://aspiringananya.github.io/portfolio/',
+     },
 ];
